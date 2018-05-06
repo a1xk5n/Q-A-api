c4894f5051eb3c2d28f38e53ee04fb8d44bfb9aa
Alex Kashkan <alexei.kashkan@gmail.com>
2018-05-06 20:18:07 +0300

    fix with user info

--- business-logic/services/userService.js
+++ business-logic/services/userService.js
@@ -41,7 +41,7 @@ class UserService {
                         },
                     );
 
-                    resolve({ token: token, userInfo });
+                    resolve({ token: token, ...userInfo });
                 })
                 .catch(error => reject(error));
         });
@@ -70,7 +70,7 @@ class UserService {
                         },
                     );
 
-                    resolve({ token: token, ...userInfo  });
+                    resolve({ token: token, ...userInfo });
                 })
                 .catch(error => reject(error));
         });
